324f30049fcac28ebeee8a85d4a6b626b694b24a
shenjiyuan <815462629@qq.com>
2023-11-12 09:00:44 +0000

    retrain

--- serverbase.py
+++ serverbase.py
@@ -538,4 +538,52 @@ class FedAvg(Server):
         
         return return_global_model
 
-         
+
+    def retrain(self):
+        self.current_num_join_clients = len(self.remaining_clients)
+        self.global_model = copy.deepcopy(self.args.model)
+        for i in range(self.global_rounds+1):
+            s_t = time.time()
+            self.selected_clients = self.remaining_clients
+            self.send_models()
+
+            if i%self.eval_gap == 0:
+                print(f"\n-------------Round number: {i}-------------")
+                print("\nEvaluate global model")
+                self.evaluate()
+
+            for client in self.selected_clients:
+                client.train()
+
+            self.receive_models()
+            # self.save_client_model(i)
+            if self.dlg_eval and i%self.dlg_gap == 0:
+                self.call_dlg(i)
+            self.aggregate_parameters()
+            # self.save_each_round_global_model(i)
+
+            self.Budget.append(time.time() - s_t)
+            print('-'*25, 'time cost', '-'*25, self.Budget[-1])
+
+            if self.auto_break and self.check_done(acc_lss=[self.rs_test_acc], top_cnt=self.top_cnt):
+                break
+
+        print("\nBest accuracy.")
+        # self.print_(max(self.rs_test_acc), max(
+        #     self.rs_train_acc), min(self.rs_train_loss))
+        print(max(self.rs_test_acc))
+        print("\nAverage time cost per round.")
+        print(sum(self.Budget[1:])/len(self.Budget[1:]))
+
+        self.save_results()
+        self.save_global_model()
+
+        if self.num_new_clients > 0:
+            self.eval_new_clients = True
+            self.set_new_clients(clientAVG)
+            print(f"\n-------------Fine tuning round-------------")
+            print("\nEvaluate new clients")
+            self.evaluate()
+        
+    
+    
